4e4f0a286901baca24877ef890f908281766bbce
sravanr788 <sravanr7882gmail.com>
2023-12-07 16:49:24 +0530

    components done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import './App.css';
 import lake from "./image/lake.JPG";
+import GalleryHeader from './components/GalleryHeader';
+import GalleryMain from './components/GalleryMain';
 import GallaryFooter from './components/GallaryFooter';
 
 const imageData = [
@@ -24,30 +26,12 @@ const imageData = [
 function App() {
   return (
     <div>
-      <header>
-        <img className="logo" src="https://s3.ap-south-1.amazonaws.com/kalvi-education.github.io/front-end-web-development/Kalvium-Logo.png" alt=""></img>
-        <nav>
-          <ul className='nav_links'>
-            <li><a href='#'>Home</a></li>
-            <li><a href='#'>About</a></li>
-            <li><a href='#'>More</a></li>
-          </ul>
-
-        </nav>
-        <a className='cta' href='#'><button>Contact</button></a>
-      </header>
-
-      <div>
-        {
-          imageData.map(image=>{
-            return(
-              <div key={image.id} className="column">
-                <img src={image.img} alt=""></img>
-              </div>
-            )
-          })
-        }
-      </div>
+        {/* adding footer component */}
+      <GalleryHeader />
+        {/* adding main component */}
+      <GalleryMain 
+      imageData={imageData}
+      />
       {/* adding footer component */}
       <GallaryFooter/> 
     </div>
